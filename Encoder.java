99caa3ec45b1468eda1d2e5703dc58cf7e896b4b
Anna1293 <110286131+Anna1293@users.noreply.github.com>
2022-08-03 13:06:55 +0300

    Lesson commit

--- Encoder.java
+++ Encoder.java
@@ -5,7 +5,7 @@ public class Encoder {
         static String symbols = "\r\n.?!,;: -()\"";
         static String numbers = "1234567890";
         static String CryptoAlphabet = russianAlphabet + englishAlphabet + symbols + numbers;
-        static char[] chars = symbols.toCharArray();
+        static char[] chars =CryptoAlphabet.toCharArray();
 
     }
 
